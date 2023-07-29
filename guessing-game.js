c01d1160f98f34b144ee5cd5727c73ad20a826f8
root <root@worklaptop>
2023-07-29 16:28:55 -0500

    pass two complete

--- guessing-game.js
+++ guessing-game.js
@@ -5,103 +5,67 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let getRandomInt = function ( min, max){
+let numAttempts = 5
+
+let getRandomInt = function (min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 };
 
-let randonInRange = function(min, max){
+let randomInRange = function ( min, max) {
     return getRandomInt(min, max)
 }
 
-// let secretNumber = randonInRange(0, 100);
+// let secretNumber = (randomInRange(min, max));
 
-let checkGuess = function(answer){
-    if(answer > el){
-        console.log("Too high");
-        return false;
-    };
-    if(answer < el){
-        console.log("Too low");
-        return false;
-    };
-    if(answer === el){
-        console.log("Correct!");
-        return true;
-    };
-};
-let enterMin = function(){
-    rl.question('Enter a min number: ', answer)
-};
-// let askGuess = function(){
-//     rl.question('Enter a guess: ', answer => {
-//         if(!(checkGuess(Number(answer)))){
-//             // checkGuess(answer);
-//             return askGuess()
-//         };
-//         rl.close()
-//     });
-
-// };
 
 let askRange = function(){
-    let num1;
-    let num2;
-    let el;
-    rl.question('Enter a max number: ', answer => {
-        num1 = Number(answer);
-        rl.question('Enter a min number: ', answer => {
-            num2 = Number(answer);
-            console.log(`I'm thinking of a number between ${num1} and ${num2}...`);
-            el = randonInRange(num1, num2);
-            let askGuess = function(){
-                rl.question('Enter a guess: ', answer => {
-                    if(!(checkGuess(Number(answer)))){
-                        // checkGuess(answer);
-                        return askGuess()
-                    };
-                    rl.close()
-                });
+    let min;
+    let max;
+    rl.question('Enter a min number: ', answer => {
+        min = Number(answer);
+        rl.question('Enter a max number: ', answer =>{
+            max = Number(answer);
+            let secretNumber = (randomInRange(min, max));
             
-            };
-            let checkGuess = function(answer){
-                if(answer > el){
-                    console.log("Too high");
-                    return false;
-                };
-                if(answer < el){
-                    console.log("Too low");
-                    return false;
-                };
-                if(answer === el){
-                    console.log("Correct!");
-                    return true;
-                };
-            };
-            askGuess()
-            
-        }
-        
-        )
-    } );
-    let randonInRange = function(num2, num1){
-        return getRandomInt(num2, num1)
+let checkGuess = function(num){
+    if(num > secretNumber){
+        console.log('Too high');
+        return false
+    };
+    if(num < secretNumber){
+        console.log('Too low');
+        return false
+    };
+    if(num === secretNumber){
+        console.log('You Win!');
+        return true
+    };
+};
+
+let askGuess = function(){
+    rl.question('Enter a guess: ', answer => {
+        if(checkGuess(Number(answer))){
+    rl.close()}else{
+        numAttempts--;
+        if(numAttempts == 0){
+            console.log('You Lose');
+            rl.close()
+        }else{
+        askGuess()}
     }
+    });
     
 }
+            console.log(`I'm thinking of a number between ${min} and ${max}...`);
+            askGuess()
+        });
+    });
+};
 
-// let askGuess = function(){
-//     rl.question('Enter a guess: ', answer => {
-//         if(!(checkGuess(Number(answer)))){
-//             // checkGuess(answer);
-//             return askGuess()
-//         };
-//         rl.close()
-//     });
-
-// };
-
-// askGuess();
-// console.log(randonInRange(0, 3))
 askRange()
+
+
+
+// askGuess()
